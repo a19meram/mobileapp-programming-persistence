3857a615862348470079e215eb9ed2c99cd54476
a19meram <a19meram@student.his.se>
2021-05-05 17:20:01 +0200

    Adding variables and hints!

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Start creating variables
+    EditText name, age, program;
+    Button read, write;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
