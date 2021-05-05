b7ee6db13eb155d193a6b08856e26e1115976b14
a19meram <a19meram@student.his.se>
2021-05-05 17:35:56 +0200

    Adding getters and toString!

--- app/src/main/java/com/example/persistence/Student.java
+++ app/src/main/java/com/example/persistence/Student.java
@@ -1,4 +1,35 @@
 package com.example.persistence;
 
 public class Student {
+    String name;
+    String program;
+    Integer age;
+
+
+    public Student(String name, String program, Integer age) {
+        this.name = name;
+        this.program = program;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", program='" + program + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
 }
