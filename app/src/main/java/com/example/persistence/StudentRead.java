253e4ab3055cddfcc5e25c0279bf6edc88a1ad89
a19meram <a19meram@student.his.se>
2021-05-06 00:27:58 +0200

    Adding a new recyclerView!

--- app/src/main/java/com/example/persistence/StudentRead.java
+++ app/src/main/java/com/example/persistence/StudentRead.java
@@ -1,14 +1,24 @@
 package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class StudentRead extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_read);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
     }
 }
