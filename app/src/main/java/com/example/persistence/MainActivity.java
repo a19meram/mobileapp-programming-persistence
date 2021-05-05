86b8958e1b8ac9afb3cd1e3ffb3c493746cb1db6
a19meram <a19meram@student.his.se>
2021-05-05 23:01:32 +0200

    creating an intent to open another activity!

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Starting the read button
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StudentRead.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
