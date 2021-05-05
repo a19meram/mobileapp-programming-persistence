3b2b2ac95dcdf822f9c4241351d231b6ba00f9a5
a19meram <a19meram@student.his.se>
2021-05-05 17:30:37 +0200

    Adding onClickListener for button and strings!

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //giving references to use them later
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        program = findViewById(R.id.program);
+        read = findViewById(R.id.read);
+        write =findViewById(R.id.write);
+
+        //after a click on write button get EditText value in string and integer
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringName = name.getText().toString();
+                String integerAge = age.getText().toString();
+                String stringProgram = program.getText().toString();
+            }
+        });
+
+
     }
 }
