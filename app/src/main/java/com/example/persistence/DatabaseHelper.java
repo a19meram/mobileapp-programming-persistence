cf7c9ef2004770ff5aedc26c685f7e7b77162c58
a19meram <a19meram@student.his.se>
2021-05-05 17:40:57 +0200

    Creating a database version from lenasys copy!

--- app/src/main/java/com/example/persistence/DatabaseHelper.java
+++ app/src/main/java/com/example/persistence/DatabaseHelper.java
@@ -1,4 +1,20 @@
 package com.example.persistence;
 
-public class DatabaseHelper {
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1; // If this is incremented onUpgrade() will be executed
+    private static final String DATABASE_NAME = "Student_database.db"; // The file name of our database
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
