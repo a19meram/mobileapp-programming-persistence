26479d00ce9f7900efdc3e3f77a96775f9dc827a
a19meram <a19meram@student.his.se>
2021-05-05 18:39:22 +0200

    Creating a table columns and table name from lenasys copy!

--- app/src/main/java/com/example/persistence/DatabaseHelper.java
+++ app/src/main/java/com/example/persistence/DatabaseHelper.java
@@ -6,7 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1; // If this is incremented onUpgrade() will be executed
-    private static final String DATABASE_NAME = "Student_database.db"; // The file name of our database
+    private static final String DATABASE_NAME = "Student_database"; // The file name of our database
+    //Table name
+    private static final String TABLE_NAME = "STUDENT";
+    //Table columns
+    static final String COLUMN_NAME_AGE = "age";
+    static final String COLUMN_NAME_NAME = "name";
+    static final String COLUMN_NAME_PROGRAM = "program";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
